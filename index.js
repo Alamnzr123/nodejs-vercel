679cce5dc04acad284ecee42678703bd341262b6
unknown <nzr.rahmad@gmail.com>
2024-12-30 10:47:36 +0700

    Second commit

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get("/hello", (req, res) => res.send("Hello Express!"));
 
+app.get("/incoming-request", (req, res) => res.send("Request Allow!"));
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
